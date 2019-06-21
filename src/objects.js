63d52f3734d99dbc24bd746be968c3997f4d5ec4
Unknown <robert.campbell@concentra.co.uk>
2019-06-21 20:44:02 +0100

    src/arrays.js added

---
+++ src/objects.js
@@ -0,0 +1,41 @@
+const arrays = require("./arrays.js")
+
+const toArrays = objects => {
+  if ("object" !== typeof objects) throw new TypeError("expecting an array")
+
+  objects = Array.isArray(objects) ? objects.slice() : [objects]
+
+  if (!objects.length) throw new Error("expecting at least one object")
+
+  const headers = [],
+    lookup = {},
+    buf = []
+
+  let index
+
+  while (objects.length) {
+    let lbuf = [],
+      object = objects.shift()
+
+    for (let key in object) {
+      if (!object.hasOwnProperty(key)) continue
+      index = lookup[key]
+      if (index === undefined) {
+        index = lookup[key] = Object.keys(lookup).length
+        headers[index] = key
+      }
+
+      lbuf[index] = object[key]
+    }
+
+    buf.push(lbuf)
+  }
+
+  buf.unshift(headers)
+
+  return buf
+}
+
+const toCSV = objects => arrays.toCSV(toArrays(objects))
+
+exports.toCSV = toCSV
